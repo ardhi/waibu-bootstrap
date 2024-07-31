d26669d0d268853b689f28af12168fa8ba57a2cb
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-31 09:09:15 +0700

    Refactor

--- waibuMpa/theme/component/btn.js
+++ waibuMpa/theme/component/btn.js
@@ -1,5 +1,10 @@
 const baseCls = 'btn'
 
+const getAttrValues = {
+  variant: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark', 'link'],
+  size: ['sm', 'md', 'lg']
+}
+
 async function btn ({ params, reply } = {}) {
   const { has, isEmpty, omit } = this._
 
@@ -11,12 +16,14 @@ async function btn ({ params, reply } = {}) {
   if (params.tag === 'a') attr.role = 'button'
   // def
   attr.class.push(baseCls)
-  for (const item of ['active']) {
-    this._hasAttr(attr, item, baseCls)
+  if (has(attr, 'toggle')) attr['data-bs-toggle'] = 'button'
+  if (has(attr, 'active')) {
+    attr.class.push('active')
+    attr['aria-pressed'] = true
   }
   const cls = `${baseCls}${has(attr, 'outline') ? '-outline' : ''}`
   for (const item of ['variant', 'size']) {
-    this._getAttr(attr, item, cls)
+    this._getAttr(attr, item, cls, getAttrValues)
   }
   if (has(attr, 'text-nowrap')) attr.class.push('text-nowrap')
   if (has(attr, 'icon')) {
@@ -31,7 +38,7 @@ async function btn ({ params, reply } = {}) {
     params.html += ' ' + await this._renderTag('i', { params: args, reply })
     delete attr['icon-end']
   }
-  const omitted = ['icon', 'icon-end', 'text-nowrap']
+  const omitted = ['icon', 'icon-end', 'text-nowrap', 'active', 'toggle']
   if (params.tag === 'a' && has(attr, 'disabled')) {
     attr.class.push('disabled')
     attr['aria-disabled'] = true

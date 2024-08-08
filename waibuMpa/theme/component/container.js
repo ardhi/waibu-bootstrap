b97f66198d611f673d45fbdc057f6e9e973581fe
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-08 17:32:24 +0700

    Add attribute: responsive

--- waibuMpa/theme/component/container.js
+++ waibuMpa/theme/component/container.js
@@ -5,9 +5,10 @@ const container = {
   handler: async function ({ params }) {
     const { has } = this._
     const attr = params.attr
-    if (has(attr, 'fluid')) this._hasAttr(attr, 'fluid', baseCls, ['fluid'])
+    if (has(attr, 'responsive')) attr.class.push(`${baseCls}-fluid`)
     else if (has(attr, 'size')) this._getAttr(attr, 'size', baseCls)
     else attr.class.push(baseCls)
+    delete attr.responsive
   }
 }
 

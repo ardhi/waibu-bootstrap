508d2c764a6443ba680b7a1725009ba553225f42
Ardhi Lukianto <ardhi@lukianto.com>
2025-01-23 20:52:21 +0700

    Updates

--- waibuMpa/theme/component/factory/app-launcher-trigger.js
+++ waibuMpa/theme/component/factory/app-launcher-trigger.js
@@ -14,22 +14,26 @@ async function appLauncherTrigger () {
         this.params.attr['data-bs-toggle'] = type === 'drawer' ? 'offcanvas' : type
         this.params.attr['aria-controls'] = id
       }
-      this.params.attr.padding = this.params.attr.padding ?? 'all-1'
+      this.params.attr.padding = this.params.attr.padding ?? 'all-0'
       this.params.attr.imgDimWidth = this.params.attr.imgDimWidth ?? 48
       this.params.attr.imgDimHeight = this.params.attr.imgDimHeight ?? 48
     }
 
     async build () {
       const { fastGlob } = this.plugin.app.bajo.lib
-      const { get, omit, merge } = this.plugin.app.bajo.lib._
+      const { get, omit } = this.plugin.app.bajo.lib._
       let logo = 'waibu'
       const files = await fastGlob(`${this.plugin.app.main.dir.pkg}/bajo/logo.*`)
       if (files.length > 0) logo = 'main'
       this.params.tag = 'a'
-      const logoStyle = get(this, `plugin.app.${logo}.config.waibu.logoStyle`)
-      if (logoStyle) this.params.attr.style = merge({}, logoStyle, this.params.attr.style)
-      this.params.html = await this.component.buildSentence(`<c:img width="${this.params.attr.imgDimWidth}" height="${this.params.attr.imgDimHeight}" src="waibuMpa:/logo/${logo}" />`)
-      this.params.attr = omit(this.params.attr, ['imgDimHeight', 'imgDimWidth'])
+      const sentence = [
+        `<c:img src="waibuMpa:/logo/${logo}"`,
+        `width="${this.params.attr.imgDimWidth}" height="${this.params.attr.imgDimHeight}"`
+      ]
+      if (this.params.attr.imgStyle) sentence.push(`style="${this.params.attr.imgStyle}"`)
+      sentence.push('/>')
+      this.params.html = await this.component.buildSentence(sentence)
+      this.params.attr = omit(this.params.attr, ['imgDimHeight', 'imgDimWidth', 'imgStyle'])
     }
   }
 }
--- waibuMpa/theme/component/factory/img.js
+++ waibuMpa/theme/component/factory/img.js
@@ -14,6 +14,21 @@ async function img () {
       }
       delete this.params.attr.holder
     }
+
+    async build () {
+      const { isString } = this.plugin.app.bajo.lib._
+      const mouseenter = []
+      const mouseleave = []
+      if (isString(this.params.attr['@mouseenter'])) mouseenter.push(...this.params.attr['@mouseenter'].split('\n'))
+      if (isString(this.params.attr['@mouseleave'])) mouseleave.push(...this.params.attr['@mouseleave'].split('\n'))
+      if (isString(this.params.attr.srcHover)) {
+        this.params.attr.srcStby = this.params.attr.src
+        mouseenter.unshift(`$el.src = $el.getAttribute('src-hover')`)
+        mouseleave.unshift(`$el.src = $el.getAttribute('src-stby')`)
+      }
+      if (mouseenter.length > 0) this.params.attr['@mouseenter'] = mouseenter.join('\n')
+      if (mouseleave.length > 0) this.params.attr['@mouseleave'] = mouseleave.join('\n')
+    }
   }
 }
 
